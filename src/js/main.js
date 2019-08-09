9e50407bae3c481d91c6a09b262f90d715022640
SineYg <sineyg324@gmail.com>
2019-08-09 16:42:41 +0300

    Сверстана и адаптирована секция brif

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,36 @@
 new WOW().init();
 $(document).ready(function(){ 
+    $('#brif-form').validate({
+        rulse: {
+            username: {
+                required: true,
+                minlength: 2
+            }, 
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages:{
+            username: {
+                required: "Укажите имя"
+                minlength: jQuery.validator.format("Имя не должно быть короче 2 символов")
+            },
+            email: "Нам нужен ваш email",
+        }
+    });
+
+    $('#brif-form').on('submit', function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: 'mail.php',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function(data) {
+                $('.success').html(data + ', Ваша форма отправлена!')
+            }
+        });
+    });
     $('#offer-form').on('submit', function(event) {
         event.preventDefault();
         $.ajax({
