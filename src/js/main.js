09d7d17c7af4d70201356469a19c96d8a8b7ce0c
SineYg <sineyg324@gmail.com>
2019-08-11 20:35:57 +0300

    Сверстаны секции steps и materials

--- src/js/main.js
+++ src/js/main.js
@@ -42,42 +42,4 @@ $(document).ready(function(){
             }
         });
     });
-    $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: $('.arrows__left'),
-        nextArrow: $('.arrows__right'), 
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    variableWidth: true,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    centerPadding: '40px',
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
 });
